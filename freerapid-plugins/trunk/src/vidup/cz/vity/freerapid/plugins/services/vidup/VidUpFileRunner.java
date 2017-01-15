c42ca9da3a0ff02d378c2b4058c02698bd3f389c
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2017-01-15 10:48:50 +0000

    VidUp: fixed, convert to xfileplayer.
    git-svn-id: http://svn.wordrider.net/svn@5616 40407085-c429-0410-a8d0-eb1a82597f6e

--- freerapid-plugins/trunk/src/vidup/cz/vity/freerapid/plugins/services/vidup/VidUpFileRunner.java
+++ freerapid-plugins/trunk/src/vidup/cz/vity/freerapid/plugins/services/vidup/VidUpFileRunner.java
@@ -1,20 +1,53 @@
 package cz.vity.freerapid.plugins.services.vidup;
 
-import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
-import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.services.xfileplayer.XFilePlayerRunner;
+import cz.vity.freerapid.plugins.webclient.MethodBuilder;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 
 import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * Class which contains main code
  *
  * @author birchie
+ * @author tong2shot
  */
-class VidUpFileRunner extends XFileSharingRunner {
+class VidUpFileRunner extends XFilePlayerRunner {
+
     @Override
-    protected List<FileSizeHandler> getFileSizeHandlers() {
-        final List<FileSizeHandler> fileSizeHandlers = super.getFileSizeHandlers();
-        fileSizeHandlers.add(new VidUpFileSizeHandler());
-        return fileSizeHandlers;
+    protected MethodBuilder getXFSMethodBuilder(String content) throws Exception {
+        MethodBuilder mb = super.getXFSMethodBuilder(content);
+        Matcher matcher = PlugUtils.matcher("'gfk',\\s*?value:\\s*?'([^']+)'", content);
+        if (!matcher.find()) {
+            throw new PluginImplementationException("'gfk' parameter not found");
+        }
+        String gfk = matcher.group(1);
+
+        matcher = PlugUtils.matcher("'_vhash',\\s*?value:\\s*?'([^']+)'", content);
+        if (!matcher.find()) {
+            throw new PluginImplementationException("'_vhash' parameter not found");
+        }
+        String _vhash = matcher.group(1);
+
+        mb.setParameter("gfk", gfk);
+        mb.setParameter("_vhash", _vhash);
+        mb.setParameter("imhuman", "");
+        return mb;
+    }
+
+    @Override
+    protected List<String> getDownloadPageMarkers() {
+        List<String> result = super.getDownloadPageMarkers();
+        result.add(0, "VideoPlayer = jwplayer(\"vplayer\")");
+        return result;
+    }
+
+    @Override
+    protected List<String> getDownloadLinkRegexes() {
+        List<String> result = super.getDownloadLinkRegexes();
+        result.add(0, "sources\\s*?:\\s*?\\[\\{\"file\":\"(http[^\"]+)\""); //avoid getting subtitle
+        return result;
     }
 }
