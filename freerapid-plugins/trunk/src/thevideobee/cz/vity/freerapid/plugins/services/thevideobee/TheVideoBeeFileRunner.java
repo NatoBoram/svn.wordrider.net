518bb0996788619c0b2c3d4bbc26495db089c258
tong2shot <tong2shot@40407085-c429-0410-a8d0-eb1a82597f6e>
2017-06-12 12:41:01 +0000

    TheVideoBee: add download link regex.
    git-svn-id: http://svn.wordrider.net/svn@5673 40407085-c429-0410-a8d0-eb1a82597f6e

--- freerapid-plugins/trunk/src/thevideobee/cz/vity/freerapid/plugins/services/thevideobee/TheVideoBeeFileRunner.java
+++ freerapid-plugins/trunk/src/thevideobee/cz/vity/freerapid/plugins/services/thevideobee/TheVideoBeeFileRunner.java
@@ -2,10 +2,19 @@ package cz.vity.freerapid.plugins.services.thevideobee;
 
 import cz.vity.freerapid.plugins.services.xfileplayer.XFilePlayerRunner;
 
+import java.util.List;
+
 /**
  * Class which contains main code
  *
  * @author birchie
  */
 class TheVideoBeeFileRunner extends XFilePlayerRunner {
+
+    @Override
+    protected List<String> getDownloadLinkRegexes() {
+        List<String> ret = super.getDownloadLinkRegexes();
+        ret.add(0, "sources\\s*?:\\s*?\\[[\"'](http[^\"']+?)[\"']");
+        return ret;
+    }
 }
